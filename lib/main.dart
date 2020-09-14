e4ee71a0bd121da0940546ec1e1efb5eb0daaf4d
kfukuda <kfukuda@elp.co.jp>
2020-09-14 19:01:57 +0900

    Firebase連携テストその20

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,16 @@
 
 import 'dart:async';
 
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'tabs_page.dart';
 
 Future<void> main() async {
-//  WidgetsFlutterBinding.ensureInitialized();
-//  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
